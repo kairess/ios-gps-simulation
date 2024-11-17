02654c783d0f3bf03b44bae6cf392faf89f0e7dc
kairess <kairess87@gmail.com>
2024-11-17 12:53:45 +0900

    tunnel wifi

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@ pymobiledevice3 usbmux list
 
 pymobiledevice3 mounter auto-mount
 
+# 터널링 시작 (유선)
 sudo python3 -m pymobiledevice3 remote tunneld
+
+# 터널링 시작 (WiFi)
+sudo python3 -m pymobiledevice3 remote start-tunnel -t wifi
 
 pymobiledevice3 developer dvt simulate-location play test.gpx
